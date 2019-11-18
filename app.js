9f12b2cb365f4323396a12b37908aec679b2cecf
sh183694@gmail.com <sh183694@gamil.com>
2019-11-19 06:40:07 +0900

    static

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
   res.send("Hello home page");
+});
+
+app.get("/login", function(req, res) {
+  res.send("<h1>Login please</h1>");
+});
+
+app.get("/1", function(req, res) {
+  res.send("Hello, <img src='/1.png'>");
 });
 
 app.listen(3000, function() {
