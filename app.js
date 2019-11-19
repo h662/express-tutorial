ffba0b2372cb0ba003eb7b8c9265d8fc3ee8f2fb
sh183694@gmail.com <sh183694@gamil.com>
2019-11-20 06:18:00 +0900

    TEST dynamic

--- app.js
+++ app.js
@@ -15,6 +15,33 @@ app.get("/1", function(req, res) {
   res.send("Hello, <img src='/1.png'>");
 });
 
+app.get("/dynamic", function(req, res) {
+  var lis = "";
+  for (var i = 0; i < 5; i++) {
+    lis = lis + "<li>coding</li>";
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>dynamic</title>
+    </head>
+    <body>
+        <h1>Hello Dynamic!</h1>
+        <ul>
+          ${lis}
+        </ul>
+        ${time}
+    </body>
+  </html>
+  `;
+  res.send(output);
+});
+
 app.listen(3000, function() {
   console.log("Connected 3000 port");
 });
